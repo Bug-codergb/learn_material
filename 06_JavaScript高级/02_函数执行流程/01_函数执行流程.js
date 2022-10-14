4e694983b88b9386e633839dd25a954c4575863c
Bug码 <70065574+Bug-codergb@users.noreply.github.com>
2022-10-14 16:22:55 +0800

    Update 01_函数执行流程.js
    文件拆分m3u8

--- 06_JavaScript高级/02_函数执行流程/01_函数执行流程.js
+++ 06_JavaScript高级/02_函数执行流程/01_函数执行流程.js
@@ -8,3 +8,23 @@ function bar(){
   foo();
 }
 bar()
+
+//m3u8
+var video = document.getElementById("video");
+      getVideo().then((data) => {
+        console.log(data);
+        var videoSrc = "http://localhost:8888/video/source/app";
+        if (Hls.isSupported()) {
+          var hls = new Hls();
+          hls.loadSource(videoSrc);
+          hls.attachMedia(video);
+        } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+          video.src = videoSrc;
+        }
+      });
+const filePath = path.resolve(__dirname, "../../", "./upload/video/a.m3u8");
+    ctx.set("content-type", "application/vnd.apple.mpegurl");
+    ctx.body = fs.createReadStream(filePath);
+    // ctx.body = {
+    //   url:"http://localhost:8888/a.m3u8"
+    // };
