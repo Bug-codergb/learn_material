4d9e58dad90c7f19cde5c327c8f9b3d829c66542
Bug码 <70065574+Bug-codergb@users.noreply.github.com>
2023-01-15 15:41:45 +0800

    网络请求相应数据处理

--- 02_dart/01_dart基本语法/01_hello_dart.dart
+++ 02_dart/01_dart基本语法/01_hello_dart.dart
@@ -1,3 +1,33 @@
 main(){
   print("hello-dart");
 }
+
+class GBResponseType{
+  final int status;
+  final dynamic data;
+  GBResponseType(this.status,this.data);
+  GBResponseType.fromMap(Map<String,dynamic> json) :data = json['data'],status = json['status'];
+}
+
+typedef DataParser<T> = T Function(Map<String,dynamic>);
+
+class GBRequest{
+  Future<GBResponseType> request(String url,{ String method='post', Map<String,dynamic> data, Map<String,dynamic> params, DataParser<T> dataparser } ) async {
+    BaseOptions baseOptions = BaseOptions({
+      baseUrl:"http://www.baidu.com",
+      connectTimeout:5000
+    })
+    
+    Dio dio Dio(baseOptions);
+    Options options = Options(method:method);
+    final res = await dio.request(url,options:options,data:data,queryParameters:params);
+    
+    final responseType = GBResponseType.fromMap(res.data);
+    if(dataParser!=null){
+      GBResponseType gbResponseType = GBResponseType(responseType.data,dataParser(responseType.status));
+      return gbResponseType;
+    }else{
+      
+    }
+  }
+}
