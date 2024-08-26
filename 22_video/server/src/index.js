080d03338c9cb8335e1a694c084de5f9eaa437bb
codergb <betweent@126.com>
2024-08-26 21:32:04 +0800

    视频range

--- 22_video/server/src/index.js
+++ 22_video/server/src/index.js
@@ -4,9 +4,31 @@ const app = express();
 const router = express.Router();
 router.get("/video", (req, res, next) => {
   const rootPath = process.cwd();
-  const filePath = path.resolve(rootPath, "./public/1.mp4")
-  res.set("content-type","video/mp4")
-  res.sendFile(filePath);
+
+  const filePath = "public/video/3.m4v"
+  const stat = fs.statSync(filePath);
+  const fileSize = stat.size;
+  const range = req.get('range');
+
+  const parts = range.replace(/bytes=/, "").split("-");
+ 
+  const start = parseInt(parts[0], 10);
+  const end = Math.min(start + 999999, fileSize - 1);
+
+  const chunkSize = (end - start) + 1;
+
+  console.log(fileSize)
+  console.log(start, end);
+  const file = fs.createReadStream(filePath,{ start, end });
+
+  res.setHeader('Content-Type', 'video/mp4'); 
+  res.setHeader("Content-Range", `bytes ${start}-${end}/${fileSize}`)
+  res.setHeader("Content-Length",chunkSize),
+  res.setHeader("Accept-Ranges", "bytes")
+
+  res.status(206);
+  return file.pipe(res);
+
 })
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
